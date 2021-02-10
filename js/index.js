34f9f2977454b0599fed07e0dfafa6d7c2fc6abf
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-10 19:35:07 +0300

    experiment

--- js/index.js
+++ js/index.js
@@ -1,56 +1,8 @@
-// =====================================================
-// ====== захардкоженно! осторожно ! гавнокод ! ========
-// ====================== тест =========================
-//
-// const sliderList = document.querySelector('.slider__list')
-// const sliderItem = document.querySelectorAll('.slider__item ')
-//
-// const prev = document.querySelector('.prev')
-// const next = document.querySelector('.next')
-//
-// const src = [
-//     'img/1.jpg',
-//     'img/2.jpg',
-//     'img/3.jpg',
-//     'img/4.jpg',
-//     'img/5.jpg',
-//     'img/6.jpg',
-// ]
-//
-// let val = 0
-//
-// prev.addEventListener('pointerdown', () => {
-//     sliderList.style.transform = `translateX(-${ val += 500 }px)`
-//     if (val >= 1000) {
-//         val = 500
-//         return
-//     }
-//     console.log(val)
-// })
-//
-// next.addEventListener('pointerdown', () => {
-//     sliderList.style.transform = `translateX(-${ val -= 500 }px)`
-//     if (val <= 0) {
-//         val = 0
-//         return
-//     }
-// })
-//
-//
-// function transrormItems(node) {
-//     let deg = 1
-//     node.forEach(item => {
-//         item.style.transform = `scale(${ deg })`
-//         deg -= 0.1
-//     })
-// }
-//
-// transrormItems(sliderItem)
-
 class Gallery {
     constructor() {
         this.sliderList = document.querySelector('.slider__list')
         this.sliderItem = null
+        this.sliderImg = null
 
         this.prev = document.querySelector('.prev')
         this.next = document.querySelector('.next')
@@ -82,9 +34,9 @@ class Gallery {
             'img/1.jpg',
             'img/2.jpg',
             'img/3.jpg',
-            'img/4.jpg',
-            'img/5.jpg',
-            'img/6.jpg',
+            // 'img/4.jpg',
+            // 'img/5.jpg',
+            // 'img/6.jpg',
         ]
     }
 
@@ -101,34 +53,71 @@ class Gallery {
     offsetImage = () => {
         console.log('[3] offsetImage')
 
-        this.sliderItem = document.querySelectorAll('.slider__item ')
+        this.sliderItem = Array.from(document.querySelectorAll('.slider__item '))
 
         this.sliderItem.forEach(item => {
             item.style.transform = `translateX(${ this.offsetImg * 250 }px)`
             this.offsetImg++
         })
-
-        console.log(this.offsetImg)
     }
 
+    draw = () => {
+        console.log('--- draw ---')
 
+        const img = document.createElement('img')
+        img.src = this.src[this.stepImg]
+        img.classList.add('slider__img')
+        img.style.transform = `translateX(${ this.offsetImg * 200 }px)`
+
+        this.sliderList.append(img)
+
+        if (this.stepImg + 1 === this.src.length) {
+            this.stepImg = 0
+        } else {
+            this.stepImg++
+        }
+
+        this.offsetImg = 1
+    }
 
     clickOnPrev = () => {
+        console.log('prev')
+
+        this.prev.removeEventListener('click', this.clickOnPrev)
+
         this.sliderImg = document.querySelectorAll('.slider__img')
-        console.log('prev',this.sliderImg)
+        let offset2 = 0
+
+        console.log(this.sliderImg)
+
+        for (let i = 0; i < this.sliderImg.length; i++) {
+            this.sliderImg[i].style.transform = `translateX(${ offset2 * 200 - 200 }px)`
+            offset2++
+        }
+
+        setTimeout(() => {
+            this.sliderImg[0].remove()
+            this.draw()
+            this.prev.addEventListener('click', this.clickOnPrev)
+        }, 20)
+        // this.draw()
+
+        // for (let i = 0; i < this.sliderItem; i++) {
+        //     this.renderTemplate(this.sliderList, this.templateImg(this.src[i]))
+        // }
     }
 
     clickOnNext = () => {
-        this.sliderImg = document.querySelectorAll('.slider__img')
-        console.log('next',this.sliderImg)
+        console.log('next',this.sliderItem)
     }
 
     init = () => {
         console.log('[0] Gallery INIT')
         this.getSrc()        // [1]
-        this.renderImage()   // [2]
-        this.offsetImage()   // [3]
-
+        // this.renderImage()   // [2]
+        // this.offsetImage()   // [3]
+        this.draw()
+        this.draw()
     }
 }
 
