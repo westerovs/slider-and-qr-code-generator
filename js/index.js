8eddbff4999622505a9e8fd4ad583c281f9d5ff8
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-11 20:49:23 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,125 +1,71 @@
-class Gallery {
-    constructor() {
-        this.sliderList = document.querySelector('.slider__list')
-        this.sliderItem = null
-        this.sliderImg = null
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    speed: 300,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 0,
+        slideShadows: false,
+    },
+});
 
-        this.prev = document.querySelector('.prev')
-        this.next = document.querySelector('.next')
 
-        this.prev.addEventListener('click', this.clickOnPrev)
-        this.next.addEventListener('click', this.clickOnNext)
 
-        this.src = null
-        this.stepImg = 0  // шаг - опред. какую картинку показывать в основном блоке
-        this.offsetImg = 0
-    }
 
-    templateImg = (src) => `
-        <li class="slider__item">
-            <img class="slider__img" src="${ src }" alt="img"/>
-        </li>`
+console.log(swiper)
 
-    renderTemplate = (container, template, position = 'beforeend') => {
-        if (container instanceof Element) {
-            container.insertAdjacentHTML(position, template)
-        }
-    }
+const wrapper = document.querySelector('.swiper-wrapper')
+const slides = Array.from(document.querySelectorAll('.swiper-slide'))
 
-    // 1
-    getSrc = () => {
-        console.log('[1] getSrc')
+let count = 0
+let arrDeg = []
 
-        this.src = [
-            'img/1.jpg',
-            'img/2.jpg',
-            'img/3.jpg',
-            // 'img/4.jpg',
-            // 'img/5.jpg',
-            // 'img/6.jpg',
-        ]
-    }
+function anim() {
+    console.log('ANIMATE')
+    let deg = 100
+    let scale = 1
 
-    // 2
-    renderImage = () => {
-        console.log('[2] render image')
+    slides.forEach((item, index) => {
+        arrDeg.push([`translateX(${ index }px) translateY(-${ index }px) translateZ(-${ index }px) scale(${ scale })`])
 
-        for (let i = 0; i < this.src.length; i++) {
-            this.renderTemplate(this.sliderList, this.templateImg(this.src[i]))
-        }
-    }
-
-    // 3
-    offsetImage = () => {
-        console.log('[3] offsetImage')
-
-        this.sliderItem = Array.from(document.querySelectorAll('.slider__item '))
-
-        this.sliderItem.forEach(item => {
-            item.style.transform = `translateX(${ this.offsetImg * 250 }px)`
-            this.offsetImg++
-        })
-    }
-
-    draw = () => {
-        console.log('--- draw ---')
-
-        const img = document.createElement('img')
-        img.src = this.src[this.stepImg]
-        img.classList.add('slider__img')
-        img.style.transform = `translateX(${ this.offsetImg * 200 }px)`
-
-        this.sliderList.append(img)
-
-        if (this.stepImg + 1 === this.src.length) {
-            this.stepImg = 0
-        } else {
-            this.stepImg++
-        }
-
-        this.offsetImg = 1
-    }
-
-    clickOnPrev = () => {
-        console.log('prev')
-
-        this.prev.removeEventListener('click', this.clickOnPrev)
-
-        this.sliderImg = document.querySelectorAll('.slider__img')
-        let offset2 = 0
-
-        console.log(this.sliderImg)
-
-        for (let i = 0; i < this.sliderImg.length; i++) {
-            this.sliderImg[i].style.transform = `translateX(${ offset2 * 200 - 200 }px)`
-            offset2++
-        }
-
-        setTimeout(() => {
-            this.sliderImg[0].remove()
-            this.draw()
-            this.prev.addEventListener('click', this.clickOnPrev)
-        }, 20)
-        // this.draw()
-
-        // for (let i = 0; i < this.sliderItem; i++) {
-        //     this.renderTemplate(this.sliderList, this.templateImg(this.src[i]))
+        item.innerText = `slider ${ index }`
+        // item.style.transform = arrDeg[index]
+        //
+        // scale -= 0.2
+        // if (scale <= 0) scale = 0
+        //
+        // if (item.classList.contains('swiper-slide-active')) {
+        //     item.style.transform = `translateX(${ index }px) translateY(-${ index }px) translateZ(-${ index }px) scale(${ 1 })`
         // }
-    }
-
-    clickOnNext = () => {
-        console.log('next',this.sliderItem)
-    }
-
-    init = () => {
-        console.log('[0] Gallery INIT')
-        this.getSrc()        // [1]
-        // this.renderImage()   // [2]
-        // this.offsetImage()   // [3]
-        this.draw()
-        this.draw()
-    }
+    })
 }
 
-const gallery = new Gallery()
-gallery.init()
+anim()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
