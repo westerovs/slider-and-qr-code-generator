5212d34bcb670b37538ddf98d5ee8ce68299b185
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-12 10:26:49 +0300

    захарÐчто-то похожее

--- js/index.js
+++ js/index.js
@@ -1,70 +1,31 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    speed: 300,
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
     },
     loop: true,
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate: 0,
-        slideShadows: false,
-    },
 });
 
 
 
-
-console.log(swiper)
-
-const wrapper = document.querySelector('.swiper-wrapper')
 const slides = Array.from(document.querySelectorAll('.swiper-slide'))
+const activeSlide = document.querySelector('.swiper-slide-active')
 
-let count = 0
-let arrDeg = []
+slides.forEach((item, index) => {
+    // item.innerText = `slider ${ index }`
 
-function anim() {
-    console.log('ANIMATE')
-    let deg = 100
-    let scale = 1
-
-    slides.forEach((item, index) => {
-        arrDeg.push([`translateX(${ index }px) translateY(-${ index }px) translateZ(-${ index }px) scale(${ scale })`])
-
-        item.innerText = `slider ${ index }`
-        // item.style.transform = arrDeg[index]
-        //
-        // scale -= 0.2
-        // if (scale <= 0) scale = 0
-        //
-        // if (item.classList.contains('swiper-slide-active')) {
-        //     item.style.transform = `translateX(${ index }px) translateY(-${ index }px) translateZ(-${ index }px) scale(${ 1 })`
-        // }
-    })
-}
-
-anim()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (item.classList.contains('swiper-slide-active')) item.style.outline = '3px dashed red'
+})
 
 
 
