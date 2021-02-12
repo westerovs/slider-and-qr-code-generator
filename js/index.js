cbdc9875480a2fae550f5d82cfde8489c8fa7383
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-12 13:54:18 +0300

    добавил асинхронность

--- js/index.js
+++ js/index.js
@@ -1,19 +1,76 @@
-const slides = document.querySelectorAll('.swiper-slide')
+const srcArr = [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+];
 
-const swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 220,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    loop: (slides.length >= 3),
-});
+// ======================================
+const sliderWrapper = document.querySelector('.swiper-wrapper')
+const templateElement = document.querySelector('.swiper-template')
+
+function createSlide(src, index) {
+    const slideTemplateElement = templateElement.content.cloneNode(true)
+
+    slideTemplateElement.querySelector('span').textContent = index
+    slideTemplateElement.querySelector('.slider-img').src = src
+
+    sliderWrapper.append(slideTemplateElement)
+}
+
+async function renderSlides() {
+    const resp = await srcArr.forEach((item, index) => {
+        createSlide(item, index)
+    })
+}
+
+async function response() {
+    await renderSlides().then(data => {
+        const slidesElement = document.querySelectorAll('.swiper-slide')
+
+        return new Swiper('.swiper-container', {
+            effect         : 'coverflow',
+            grabCursor     : true,
+            centeredSlides : true,
+            slidesPerView  : 'auto',
+            coverflowEffect: {
+                rotate      : 0,
+                stretch     : 220,
+                depth       : 100,
+                modifier    : 1,
+                slideShadows: true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            loop: (slidesElement.length >= 3),
+        });
+    })
+}
+
+response()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
