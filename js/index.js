b548bbe67778e0c3f56ccbb1a0bb3ed94206d117
Alexander_LIVE <aleksandr.vesterovskiy@neuro-city.ru>
2021-02-12 11:22:14 +0300

    почти законченный резуольтат

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
+const slides = document.querySelectorAll('.swiper-slide')
+
 const swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
+        rotate: 0,
+        stretch: 220,
         depth: 100,
         modifier: 1,
         slideShadows: true,
@@ -13,20 +15,5 @@ const swiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
     },
-    loop: true,
+    loop: (slides.length >= 3),
 });
-
-
-
-const slides = Array.from(document.querySelectorAll('.swiper-slide'))
-const activeSlide = document.querySelector('.swiper-slide-active')
-
-slides.forEach((item, index) => {
-    // item.innerText = `slider ${ index }`
-
-    if (item.classList.contains('swiper-slide-active')) item.style.outline = '3px dashed red'
-})
-
-
-
-
